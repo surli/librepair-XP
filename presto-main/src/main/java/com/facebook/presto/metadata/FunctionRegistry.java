00c77d5a60b782fa2b13fa97f686cab74fc7162d
Haozhun Jin <haozhun.jin@gmail.com>
2017-03-19 15:04:14 -0700

    Give TypeManager a reference to FunctionManager

--- presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
+++ presto-main/src/main/java/com/facebook/presto/metadata/FunctionRegistry.java
@@ -146,6 +146,7 @@ import com.facebook.presto.type.TimeWithTimeZoneOperators;
 import com.facebook.presto.type.TimestampOperators;
 import com.facebook.presto.type.TimestampWithTimeZoneOperators;
 import com.facebook.presto.type.TinyintOperators;
+import com.facebook.presto.type.TypeRegistry;
 import com.facebook.presto.type.UnknownOperators;
 import com.facebook.presto.type.VarbinaryOperators;
 import com.facebook.presto.type.VarcharOperators;
@@ -576,6 +577,10 @@ public class FunctionRegistry
         }
 
         addFunctions(builder.getFunctions());
+
+        if (typeManager instanceof TypeRegistry) {
+            ((TypeRegistry) typeManager).setFunctionRegistry(this);
+        }
     }
 
     public final synchronized void addFunctions(List<? extends SqlFunction> functions)
