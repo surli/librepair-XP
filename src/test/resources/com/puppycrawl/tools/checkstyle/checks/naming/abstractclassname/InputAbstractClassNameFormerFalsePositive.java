4b211b37b2f7bc9a8c15ef558a6c34f19407026b
vasilyeva <vasilyeva@rutarget.ru>
2017-03-28 00:24:20 +0300

    Issue #4106: split and organized Checkstyle inputs by Test for AbstractClassName

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/abstractclassname/InputAbstractClassNameFormerFalsePositive.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/naming/abstractclassname/InputAbstractClassNameFormerFalsePositive.java
@@ -1,12 +1,12 @@
-package com.puppycrawl.tools.checkstyle.checks.naming;
+package com.puppycrawl.tools.checkstyle.checks.naming.abstractclassname;
 
 public class InputAbstractClassNameFormerFalsePositive
 {
     class Abstract {
-        
+
     }
-    
+
     class AbstractClass {
-        
+
     }
 }
