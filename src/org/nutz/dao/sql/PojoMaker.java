dd091cdd3bc2e14f3ea33af9f12fa346a4b3c59d
Howe Chiang <howechiang@gmail.com>
2017-04-07 08:58:28 +0800

    写错单词了

---
+++ src/org/nutz/dao/sql/PojoMaker.java
@@ -0,0 +1,25 @@
+package org.nutz.dao.sql;
+
+import org.nutz.dao.entity.Entity;
+
+public interface PojoMaker {
+
+    Pojo makePojo(SqlType type);
+
+    Pojo makeInsert(Entity<?> en);
+
+    Pojo makeUpdate(Entity<?> en, Object refer);
+
+    Pojo makeQuery(Entity<?> en);
+
+    Pojo makeQuery(String tableName);
+    
+    Pojo makeQuery(String tableName, String fields);
+
+    Pojo makeDelete(Entity<?> en);
+
+    Pojo makeDelete(String tableName);
+
+    Pojo makeFunc(String tableName, String funcName, String colName);
+
+}
