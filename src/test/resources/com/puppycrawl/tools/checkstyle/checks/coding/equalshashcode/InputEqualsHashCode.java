18eabe761a4884218146adef642685d67a052b62
slava ganyaev <slava.ganyaev@gmail.com>
2017-04-12 14:34:46 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for EqualsHashCodeCheckTest

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCode.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/equalshashcode/InputEqualsHashCode.java
@@ -1,38 +1,38 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.equalshashcode;
 
 public class InputEqualsHashCode {
     public boolean notEquals() {
         return true;
     }
-    
+
     public boolean equals() {
         return false;
     }
-    
+
     public boolean equals(Object o1) {
         return false;
     }
-    
+
     private boolean equals(Object o1, Object o2) {
         return false;
     }
-    
+
     private boolean equals(String s) {
         return false;
     }
-    
+
     protected int notHashCode() {
         return 1;
     }
-    
+
     public int hashCode() {
         return 1;
     }
-    
+
     public int hashCode(Object o1) {
         return 1;
     }
-    
+
     private int hashCode(Object o1, Object o2) {
         return 1;
     }
