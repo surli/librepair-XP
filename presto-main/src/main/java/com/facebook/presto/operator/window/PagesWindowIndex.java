zhdk123456/presto

Enable PagesWindowIndex retrieving Object stored as Block

presto-main/src/main/java/com/facebook/presto/operator/window/PagesWindowIndex.java
@@ -87,6 +87,12 @@ public class PagesWindowIndex
     }
 
     @Override
+    public Block getObject(int channel, int position)
+    {
+        return pagesIndex.getObject(channel, position(position));
+    }
+
+    @Override
     public void appendTo(int channel, int position, BlockBuilder output)
     {
         pagesIndex.appendTo(channel, position(position), output);

surli/librepair-XP

presto-main/src/main/java/com/facebook/presto/operator/window/PagesWindowIndex.java
@@ -87,6 +87,12 @@ public class PagesWindowIndex
     }
 
     @Override
+    public Object getObject(int channel, int position)
+    {
+        return pagesIndex.getObject(channel, position(position));
+    }
+
+    @Override
     public void appendTo(int channel, int position, BlockBuilder output)
     {
         pagesIndex.appendTo(channel, position(position), output);